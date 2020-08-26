b7d54bd0ef027bc234d9b93462d12bc1238857ed
lisa benato <lisa.benato@cern.ch>
2020-08-26 11:50:41 +0200

    Added FCN_constituents, added particle_net_jet

--- tf_keras_model.py
+++ tf_keras_model.py
@@ -154,6 +154,61 @@ def _particle_net_base(points, features=None, mask=None, setting=None, name='par
         else:
             return pool
 
+def _particle_net_base_jet(points, features=None, mask=None, jetvars=None, setting=None, name='particle_net_jet', contains_angle=False):
+    # points : (N, P, C_coord)
+    # features:  (N, P, C_features), optional
+    # mask: (N, P, 1), optinal
+
+    with tf.name_scope(name):
+        if features is None:
+            features = points
+
+        if jetvars is not None:
+            evt = keras.layers.Dense(64, activation='relu', name="L64")(jetvars)
+            evt = keras.layers.Dropout(0.2, name="DO64")(evt)
+            evt = keras.layers.Dense(32, activation='relu', name="L32")(evt)
+            evt = keras.layers.Dropout(0.2, name="DO32")(evt)
+            evt = keras.layers.Dense(16, activation='relu', name="L16")(evt)
+            evt = keras.layers.Dropout(0.2, name="DO16")(evt)
+            evt = keras.layers.Dense(8, activation='relu', name="L8")(evt)
+            evt = keras.layers.Dropout(0.2, name="DO8")(evt)
+
+        if mask is not None:
+            #Important! Our mask value is pt, that is padded as -1 for empty points.
+            #Instead of comparing if mask is equal to 0, compare if it's larger than 0.
+            mask = tf.cast(tf.greater(mask, 0), dtype='float32')  # 1 if valid
+            coord_shift = tf.multiply(999., tf.cast(tf.less_equal(mask, 0), dtype='float32'))  # make non-valid positions to 99
+
+        fts = tf.squeeze(keras.layers.BatchNormalization(name='%s_fts_bn' % name)(tf.expand_dims(features, axis=2)), axis=2)
+        for layer_idx, layer_param in enumerate(setting.conv_params):
+            K, channels = layer_param
+            pts = tf.add(coord_shift, points) if layer_idx == 0 else tf.add(coord_shift, fts)
+            if layer_idx != 0 and contains_angle:
+                contains_angle = False
+
+            fts = edge_conv(pts, fts, setting.num_points, K, channels, with_bn=True, activation='relu',
+                            pooling=setting.conv_pooling, name='%s_%s%d' % (name, 'EdgeConv', layer_idx),contains_angle=contains_angle)
+
+        if mask is not None:
+            fts = tf.multiply(fts, mask)
+
+        pool = tf.reduce_mean(fts, axis=1)  # (N, C)
+
+        if jetvars is not None:
+            pool = tf.keras.layers.Concatenate(axis=-1)([pool, evt])
+
+        if setting.fc_params is not None:
+            x = pool
+            for layer_idx, layer_param in enumerate(setting.fc_params):
+                units, drop_rate = layer_param
+                x = keras.layers.Dense(units, activation='relu')(x)
+                if drop_rate is not None and drop_rate > 0:
+                    x = keras.layers.Dropout(drop_rate)(x)
+            out = keras.layers.Dense(setting.num_class, activation='softmax')(x)
+            return out  # (N, num_classes)
+        else:
+            return pool
+
 
 class _DotDict:
     pass
@@ -189,6 +244,47 @@ def get_particle_net(num_classes, input_shapes, contains_angle=False):
     outputs = _particle_net_base(points, features, mask, setting, name='ParticleNet', contains_angle=contains_angle)
 
     return keras.Model(inputs=[points, features, mask], outputs=outputs, name='ParticleNet')
+
+def get_particle_net_jet(num_classes, input_shapes, contains_angle=False):
+    r"""ParticleNet model from `"ParticleNet: Jet Tagging via Particle Clouds"
+    <https://arxiv.org/abs/1902.08570>`_ paper.
+    Parameters
+    ----------
+    num_classes : int
+        Number of output classes.
+    input_shapes : dict
+        The shapes of each input (`points`, `features`, `mask`).
+    """
+    setting = _DotDict()
+    setting.num_class = num_classes
+    # conv_params: list of tuple in the format (K, (C1, C2, C3))
+    setting.conv_params = [
+        (16, (64, 64, 64)),
+        (16, (128, 128, 128)),
+        (16, (256, 256, 256)),
+        ]
+    # conv_pooling: 'average' or 'max'
+    setting.conv_pooling = 'average'
+    # fc_params: list of tuples in the format (C, drop_rate)
+    setting.fc_params = [(256, 0.1)]
+    setting.num_points = input_shapes['points'][0]
+
+    points = keras.Input(name='points', shape=input_shapes['points'])
+    features = keras.Input(name='features', shape=input_shapes['features']) if 'features' in input_shapes else None
+    mask = keras.Input(name='mask', shape=input_shapes['mask']) if 'mask' in input_shapes else None
+    jetvars = keras.Input(name='jetvars', shape=input_shapes['jetvars']) if 'jetvars' in input_shapes else None
+
+    outputs = _particle_net_base_jet(points, features, mask, jetvars, setting, name='ParticleNetJet', contains_angle=contains_angle)
+
+    inputs = [points]
+    if 'features' in input_shapes:
+        inputs.append(features)
+    if 'mask' in input_shapes:
+        inputs.append(mask)
+    if 'jetvars' in input_shapes:
+        inputs.append(jetvars)
+
+    return keras.Model(inputs=inputs, outputs=outputs, name='ParticleNetJet')
 
 
 def get_particle_net_lite(num_classes, input_shapes, contains_angle=False):
@@ -242,16 +338,16 @@ def get_FCN_jets(num_classes, input_shapes):
     #model.add(keras.layers.Dense(num_classes, activation='softmax'))
 
     #model 2, accuracy of training drops for some reasons...
-    #model = keras.models.Sequential(name="FCN")
-    #model.add(keras.layers.Dense(64, input_shape = input_shapes, activation='relu'))
-    #model.add(keras.layers.Dropout(rate=0.1))
-    #model.add(keras.layers.Dense(32, activation='relu'))
-    #model.add(keras.layers.Dropout(rate=0.1))
-    #model.add(keras.layers.Dense(16, activation='relu'))
-    #model.add(keras.layers.Dropout(rate=0.1))
-    #model.add(keras.layers.Dense(8, activation='relu'))
-    #model.add(keras.layers.Dropout(rate=0.1))
-    #model.add(keras.layers.Dense(num_classes, activation='softmax'))
+    model = keras.models.Sequential(name="FCN")
+    model.add(keras.layers.Dense(64, input_shape = input_shapes, activation='relu'))
+    model.add(keras.layers.Dropout(rate=0.1))
+    model.add(keras.layers.Dense(32, activation='relu'))
+    model.add(keras.layers.Dropout(rate=0.1))
+    model.add(keras.layers.Dense(16, activation='relu'))
+    model.add(keras.layers.Dropout(rate=0.1))
+    model.add(keras.layers.Dense(8, activation='relu'))
+    model.add(keras.layers.Dropout(rate=0.1))
+    model.add(keras.layers.Dense(num_classes, activation='softmax'))
 
     #model 3 is model 2 w/o dropout
     #model = keras.models.Sequential(name="FCN")
@@ -346,23 +442,23 @@ def get_FCN_jets(num_classes, input_shapes):
     #model.add(keras.layers.Dense(num_classes, activation='softmax'))
 
     #model 9, is model 6 w/0 Dropout and only LayerNormalization
-    model = keras.models.Sequential(name="FCN")
-    model.add(keras.layers.Dense(64, input_shape = input_shapes, activation='relu'))
-    #model.add(keras.layers.Dropout(rate=0.1))
-    model.add(keras.layers.LayerNormalization())
-    model.add(keras.layers.Dense(32, activation='relu'))
-    #model.add(keras.layers.Dropout(rate=0.1))
-    model.add(keras.layers.LayerNormalization())
-    model.add(keras.layers.Dense(16, activation='relu'))
-    #model.add(keras.layers.Dropout(rate=0.1))
-    model.add(keras.layers.LayerNormalization())
-    model.add(keras.layers.Dense(8, activation='relu'))
-    #model.add(keras.layers.Dropout(rate=0.1))
-    model.add(keras.layers.LayerNormalization())
-    model.add(keras.layers.Dense(4, activation='relu'))
-    #model.add(keras.layers.Dropout(rate=0.1))
+    #model = keras.models.Sequential(name="FCN")
+    #model.add(keras.layers.Dense(64, input_shape = input_shapes, activation='relu'))
+    ##model.add(keras.layers.Dropout(rate=0.1))
     #model.add(keras.layers.LayerNormalization())
-    model.add(keras.layers.Dense(num_classes, activation='softmax'))
+    #model.add(keras.layers.Dense(32, activation='relu'))
+    ##model.add(keras.layers.Dropout(rate=0.1))
+    #model.add(keras.layers.LayerNormalization())
+    #model.add(keras.layers.Dense(16, activation='relu'))
+    ##model.add(keras.layers.Dropout(rate=0.1))
+    #model.add(keras.layers.LayerNormalization())
+    #model.add(keras.layers.Dense(8, activation='relu'))
+    ##model.add(keras.layers.Dropout(rate=0.1))
+    #model.add(keras.layers.LayerNormalization())
+    #model.add(keras.layers.Dense(4, activation='relu'))
+    ##model.add(keras.layers.Dropout(rate=0.1))
+    ##model.add(keras.layers.LayerNormalization())
+    #model.add(keras.layers.Dense(num_classes, activation='softmax'))
 
     return model
 
